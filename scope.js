c2ed9e5c36a7ea872254d5605f36dbf1b5bf3c81
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-19 17:28:52 +0530

    Create scope.js

---
+++ scope.js
@@ -0,0 +1,35 @@
+ types of scope variable
+
+
+{// local scope
+let a= 10;
+    console.log(a)// local variable
+}
+
+// console.log(a)//  it will show error cause let is having block scope
+
+
+function msg(){
+var i = 10;/// function scope
+console.log(i)
+}
+// console.log(i)/// it will show error cause having function scope
+msg()
+
+
+/// global scope
+
+const x=10;/// global variable
+let y=10;
+var z=10;// global variable
+function add(){
+    sum=x+y+z;
+    console.log(sum)
+}
+add();
+
+
+
+
+
+
