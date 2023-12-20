12ecd0fb198966b39115042c096640f046c172bf
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-20 19:04:54 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,10 @@
+// string  indexing
+let firstname ="anjali";
+ // a n j a l i
+ // 0 1 2 3 4 5
+console.log(firstname[4]);
+
+// length of  string
+//  first name .length
+console.log(firstname.length-1)
+//last index: length -1
