54bc67645bf9601a2b0254af366b79aa44bca24d
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-21 17:56:15 +0530

    Create forEachmap.js

---
+++ forEach.js
@@ -0,0 +1,19 @@
+let a = [1,2,3,4];
+let b = a.forEach(function(el){
+    console.log(el*2);
+    // return el*2;
+})
+console.log(a)
+console.log(b)
+// it doesnt return values  it jut itrate the  element
+
+///map
+
+
+let a = [1,2,3,4];
+let b = a.map(function(el){
+    console.log(el*2);
+    return el*2;
+})
+console.log(a)
+console.log(b)
