f50bd74ac969c75aa32b8042cea08f94998ea2ab
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-21 11:45:09 +0530

    Create  loops.js

---
+++ loops.js
@@ -0,0 +1,34 @@
+// loops in js
+let i=1;// i
+// for(i=0;i<=10;i++){
+//     console.log("im watashi");
+// }
+
+
+// while(i<20)
+// console.log("giuniu");
+//
+do{
+
+    console.log("itsumo")
+    i++
+}
+while(i<=10)
+
+
+// for in  i loop  we use for accesing the properties of an object and array
+let person={fname:"mr mahabeer singh bartwal",mname:"mrs urmila  devi",add:"rishikesh"}
+for(let values in person){
+    if(values=='fname'){
+        console.log(person[values])
+    }
+// console.log(person[values])
+
+}
+
+ const anjali={language:" eigo,nihongo", watashitachi:"we", kurasu:"bsc"}
+ for(let values in anjali)
+ {
+    //   console.log(anjali[values])
+        console.log(values)// for key
+ }
