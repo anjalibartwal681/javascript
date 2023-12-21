5198a24c20873301e487e4f1e44ca3fc62d905e2
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-21 17:42:39 +0530

    Update map.js

--- map.js
+++ map.js
@@ -8,4 +8,22 @@ let narr= arr.map(function(value){
     console.log(value)
 })
 
+//for each
+const fruits = ["apple", "orange", "cherry"];
+fruits.forEach(myFunction);
+//The forEach() method calls a function for each element in an array.
+
+
+
+//map
+const numbers = [65, 44, 12, 4];
+const newArr = numbers.map(myFunction);
+
+document.getElementById("demo").innerHTML = newArr;
+
+function myFunction(num) {
+  return num * 10;
+}
+
+
 
