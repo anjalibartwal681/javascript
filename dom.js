656631bdcbbb0c7fa5d8d0c43e20c6a3262ecbc7
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-21 16:39:44 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,30 @@
+// What is the HTML DOM?
+// The HTML DOM is a standard object model and programming interface for HTML. It defines:
+
+// The HTML elements as objects
+// The properties of all HTML elements
+// The methods to access all HTML elements
+// The events for all HTML elements
+// In other words: The HTML DOM is a standard for how to get, change, add, or delete HTML elements.
+
+
+// html  dom can be  accessed with javascript
+// in the dom all html elements  are defined as a objects
+// a property is value that you can get and set(like changing the content of html elements)
+//method  you can  do like( add and delete  html element)
+
+<div id="dev">jhbhjj</div>
+
+<style>
+.dev{
+  background-color:"red"
+}
+</style>
+
+
+<javascript>
+a= document.getElementById("dev").innerHTML="kkjkshs"
+</javascript>
+
+
+
