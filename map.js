97daf7a521e86e6e8a96981f62141d4aa23e4e9f
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-21 17:38:48 +0530

    Create map.js

---
+++ map.js
@@ -0,0 +1,11 @@
+// map() creates a new array from calling a function for every array element.
+
+// map() does not execute the function for empty elements.
+
+// map() does not change the original array.
+  let arr=[10,20,30,40]
+let narr= arr.map(function(value){
+    console.log(value)
+})
+
+
