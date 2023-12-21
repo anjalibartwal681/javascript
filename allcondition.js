c161af5d4123b8aa307c5fd9fb9022f3fe7575e8
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-21 15:48:00 +0530

    Create allcondition.js

---
+++ allcondition.js
@@ -0,0 +1,19 @@
+// all condtional statement
+let a= prompt("entereny no")
+console.log(a)
+if(a<=30){
+ console.log("chotte matte")
+}
+ if(a<=25){
+ console.log("mimasu")
+ }
+ if(a==20){
+ console.log("mitte kudasai")
+ }else if(a<15)
+ {
+ console.log("hajime ni shimasu")
+ }
+ 
+ else{
+      console.log("hajime o shimasu")
+ }
