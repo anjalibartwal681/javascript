a5e43d4284a2fcd2850a5269de36145e19610fe2
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-23 13:33:04 +0000

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,39 @@
+// switch
+
+
+// let day = 2
+
+// switch(2){
+//     case 0:
+//         default:
+    
+// }
+let day = +prompt("enter any no between 0 to 6")
+
+switch(day){
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuseday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+         case 4:
+        console.log("thrusday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday")
+        break;
+        
+            default:
+    console.log("invalid day")
+  
+}
