eeb82fd16b02fef2898bf4110b92d6b025aaa359
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-23 12:31:31 +0000

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,25 @@
+let marks= + prompt("enter any no");
+
+if(marks===90){
+    console.log("your grade is A*");
+}
+
+else if(marks >80 && marks>70){
+    console.log("your grade is A ");
+}
+
+
+else if(marks >50 && marks<60){
+    console.log("your grade is B")
+}
+
+
+else if(marks >40 && marks<50){
+    console.log("your grade is c")
+}
+
+else if(marks >=33){
+    console.log("you are  grade is d")
+}
+else
+  console.log("you are fail")
