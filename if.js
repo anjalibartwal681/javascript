5b43874a7a718cd48d22aa8796dc036bea61c35b
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-23 12:17:22 +0000

    Create if.js

---
+++ if.js
@@ -0,0 +1,14 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+//program for and operater 
+//   a = "anjali"
+//   age=20
+//   if(a[0]==="a" && age>23)
+//   {
+//       console.log("name 2nd latter is n and age is 23")
+//   }
+   
+//   else{
+//       console.log("namae")
+//   }
+   
