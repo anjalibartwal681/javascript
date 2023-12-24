451f8a4ecae3dee78d4a089d8ff6c4dd7881185c
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-24 16:42:12 +0000

    Create array.js

---
+++ array.js
@@ -0,0 +1,43 @@
+// arrays
+// collection of Element 
+//ordered collection of item
+
+// let  fruits=["apple","mango","banana"]
+// console.log(fruits[1])
+// fruits[1]="grapes"
+// console.log(fruits[1])
+// console.log(typeof fruits)
+// console.log(Array.isArray(fruits))
+
+//push pop
+let  fruits=["apple","mango","banana"]
+console.log(fruits)
+//  fruits.push("orange")
+// console.log(fruits)
+//  poopedfruits= fruits.pop()// also return
+// console.log(fruits)
+// console.log( poopedfruits)
+
+
+
+//unshift   add from start
+// fruits.unshift("grapes")
+// fruits.unshift("grapesgooo")
+// console.log(fruits) 
+
+
+// //shift  remove  element from start
+// fruits.shift()
+// console.log(fruits)
+
+
+// push and pop are fast as compare to shift and unshift
+
+
+primitive type and reference type
+
+
+
+
+
+
