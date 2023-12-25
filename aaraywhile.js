cca7f954478baf5ebf173fae37f66683782b1180
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-25 17:08:05 +0000

    Create aaraywhile.js

---
+++ aaraywhile.js
@@ -0,0 +1,16 @@
+//for loop in array
+// use const for making aarray
+
+let fruits=["mango","grapes","orange","banana"]
+
+// console.log(fruits)
+// let fruits2 = [];
+// for(i=0;i<=fruits.length-1;i++){
+// fruits2.push(fruits[i].toUpperCase());}
+// console.log(fruits2)
+//while loop in array
+let i=0;
+while(i<fruits.length){
+console.log(fruits[i].toLowerCase());
+i++;}
+ 
