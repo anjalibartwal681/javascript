cc6fc8e8272831f25c4c14c80624893e44232de6
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-26 11:20:30 +0000

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -5,3 +5,63 @@ console.log(a+="*")
 
     
 }
+
+// pattern program
+//*
+// let row=5;
+// let a=''
+// for(let i=1;i<=row;i++){
+// console.log(a+="*")
+
+    
+// }
+
+//  let row=5;
+// let a=''
+
+// for(let i=row;i>=1;i--){
+
+// console.log(a+="*")
+    
+// }
+
+
+// let row=5
+//  for(let i=row;i>=1;i--){
+// for(let j=i;j>=1;j--){
+
+// console.log("*")
+ 
+// console.log("\n")    
+//  }}
+// // **
+// let row1=5
+
+//  for(let i=row1;i>=1;i--){
+//      let a=''
+
+//     for(let j=i;j>=1;j--){
+//      a+='* '
+//     }
+//         console.log(a)
+// }
+// let ro1=5
+// for(k=1;k<=ro1;k++){
+//     let a='';
+//     for(let i=1;i<=ro1;i++){
+//         console.log(a+='*')
+//     }
+
+//  for(let i=0;i<=ro1;i++){
+     
+//      let a=''
+
+//     // for(let j=i;j>=1;j--){
+//     for(let j=1;j<=i;j++){
+//      a+='* '
+//     }
+//         console.log(a)
+// }
+}
+
+
