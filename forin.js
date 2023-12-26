8fbe185730119fa9364759a9f696dc6f56c00786
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-26 11:57:13 +0000

    Create forin.js

---
+++ forin.js
@@ -0,0 +1,13 @@
+arr=["banana","orange","mango","papaya"]
+arr2=[]
+// for(i in arr){
+//     arr2.push(arr[i])
+// }
+// console.log(arr2)
+
+//for in return index
+
+for(i=0;i<=arr.length-1;i++){
+    arr2.push(arr[i])
+}
+console.log(arr2)
