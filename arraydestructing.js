bf16e80b521ee802a4449e6339cc51a06da7639e
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-28 16:25:23 +0000

    Create arraydestructing.js

---
+++ arraydestructing.js
@@ -0,0 +1,46 @@
+// let arr = [3, 5,"+", 7, 9];
+// let arr2=[]
+// let result = arr[0]+arr[1]
+// console.log(result)
+// if(arr.value=="+"){
+    
+// ;}
+// arr2.push(result)
+// console.log(arr2);  
+
+
+// a=prompt("enter value")
+// b=prompt("enter value")
+// c=prompt("enter value")
+
+
+let arr=[1,"+",3]
+let[input1,input2,input3]=arr
+// let arr1=[]
+// let arr2=[]
+// let arr3=[]
+// arr1.push(a)
+// console.log(arr1)
+// arr2.push(b)
+// console.log(arr2)
+// arr3.push(c)
+// console.log(arr3)
+
+// let [input1]=arr1
+// let [input2]=arr2
+// console.log( typeof arr2 )
+// let [input3]=arr3
+console.log("1st input",input1)
+console.log("1st input",input2)
+console.log("1st input",input3)
+if(input2 ==='-'){
+d= parseInt(input1)+parseInt(input3)
+console.log("total",d)
+
+    console.log("im good")
+}
+
+
+
+
+
