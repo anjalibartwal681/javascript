621e25ca18550c1a934e01dd824754ae1b5af4ec
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-28 12:18:47 +0000

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+
+
+<form name="calculator">
+            <input type="text" id="result" name="result">
+            <button type="button" onclick="funclick('1')">1</button>
+            <button type="button" onclick="funclick('2')">2</button>
+            <button type="button" onclick="funclick('1')">1</button>
+            <button type="button" onclick="funclick('2')">2</button>
+            <button type="button" onclick="funclick('+')">+</button>
+            <button type="button" onclick="funclick('-')">-</button>
+             <button type="button" onclick="funclick('*')">*</button>
+             <button type="button" onclick="funclick('/')">/</button>
+            <!--<button type="button" onclick="add();sub(); multi();">=</button>-->
+              <button type="button" onclick="calculation();">=</button>
+            <button type="button" onclick="clearall()">clear all</button>
+        </form>
+    </div>
+    <script>
+        function funclick(value) {
+            document.calculator.result.value += value;
+        }
+
+       function  clearall(){
+     document.calculator.result.value="";
+        }
+        function calculation(){
+          
+    //       let a="507";
+
+
+   
+
+    // let values = a.split('0');
+    // if (values.length === 2) {
+    //     let firstValue = parseInt(values[0]);
+    //     let secondValue = parseInt(values[1]);
+    //     let result = firstValue + secondValue;
+    //     console.log(result);
+    // }
+    
+  let inputv=document.calculator.result.value
+
+   
+var values = inputv.split('+');
+    if (values.length === 2) {
+        let firstValue = parseInt(values[0]);
+        let secondValue = parseInt(values[1]);
+        let result = firstValue + secondValue;
+        console.log(result);
+        document.calculator.result.value=result
+}
+    else {
+        values = inputv.split('-')
+        if (values.length === 2){
+      let firstValue=parseInt(values[0])
+      let secondValue= parseInt(values[1])
+      let result =firstValue-secondValue;
+      document.calculator.result.value=result}
+   
+    
+    //     else {
+    //   var values = inputv.split('*')
+    //     if (values.length === 2){
+    //   let firstValue=parseInt(values[0])
+    //   let secondValue= parseInt(values[1])
+    //   let result =firstValue*secondValue;
+    //   document.calculator.result.value=result}
+    // }   
+        
+    
+    else{
+         values = inputv.split("*")
+      if(values.length ===2){
+        let firstValue=parseInt(values[0])
+        let secondValue=parseInt(values[1])
+        let result= firstValue*secondValue;
+        document.calculator.result.value=result
+      }
+    
+    
+      else{
+    values=inputv.split("/")
+        if(values.length===2){
+          let firstValue=parseInt(values[0])
+          let secondValue=parseInt(values[1])
+          let result = firstValue / secondValue;
+          document.calculator.result.value=result
+        }
+      }
+    }
+}}
+    
+
+
+        
+        
+    
+    </script>
+</body>
+</html>
