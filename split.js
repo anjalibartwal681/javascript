7f848d9d4cdfc12bd99e0543b3aa0ea2ae0dfbef
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-28 10:05:52 +0000

    Create split.js

---
+++ split.js
@@ -0,0 +1,12 @@
+let a="507";
+
+
+   
+
+    let values = a.split('0');
+    if (values.length === 2) {
+        let firstValue = parseInt(values[0]);
+        let secondValue = parseInt(values[1]);
+        let result = firstValue + secondValue;
+        console.log(result);
+    }
