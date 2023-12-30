e30286b554713a80d77745525785fb5e2b3aa7ba
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-30 15:28:10 +0000

    Create for each.js

---
+++ for each.js
@@ -0,0 +1,24 @@
+        
+let text = "";
+const fruits = ["5", "6","+", "7"];
+fruits.forEach(myFunction);
+
+
+ 
+function myFunction(item,index) {
+  text += item + "<br>";
+
+// // //   a=fruits.indexOf('+')
+// let a=fruits
+// // let c = a.split('+')
+// // console.log(a)
+// console.log(fruits.includes('+'));
+  
+let  b = parseInt(item)
+  console.log("value", b) 
+  
+
+  
+  
+  
+}
