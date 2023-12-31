66ef5b38151ea9f68def20c7496d23d9cd932a4b
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-31 07:45:14 +0000

    Create obj-indiside-array.js

---
+++ obj-indiside-array.js
@@ -0,0 +1,19 @@
+//object inside array
+//very useful in realworld
+const users=[{name:"anjali",sec:"bscit",live:"rishikesh"}
+,{name:"anjali",sec:"bscit",live:"rishikesh"}
+,{name:"anjali",sec:"bscit",live:"rishikesh"}]
+for(let user of users){
+    console.log(user)
+       console.log(user.name)
+} 
+
+
+
+///output
+{ name: 'anjali', sec: 'bscit', live: 'rishikesh' }
+anjali
+{ name: 'anjali', sec: 'bscit', live: 'rishikesh' }
+anjali
+{ name: 'anjali', sec: 'bscit', live: 'rishikesh' }
+anjali
