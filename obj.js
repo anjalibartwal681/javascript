b0839a4e4bc85ab6490def3ba028fa393026420a
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-31 05:54:52 +0000

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,23 @@
+// computed  properties 
+const key1="objectkey1"
+const key2="objectkey2"
+
+const value1 ="myvalue1";
+const value2="myvalue2";
+//  const obj={
+//      objkey1 :"myvalue1", objectkey2:"mayvalue2"
+//  }
+
+ 
+ 
+//  const obj={
+//      [key1] : value1, 
+//      [key2] : value2
+//       }
+//   console.log(obj)
+
+
+const obj={};
+obj[key1]= value1;
+obj[key2]= value2;
+console.log(obj);
