78096f55d4326eb151256ce743f60117a04c8d1b
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2023-12-31 17:34:29 +0000

    Create fun.js

---
+++ fun.js
@@ -0,0 +1,18 @@
+function sumThreeNumber(num1,num2,num3){
+   return num1+num2+num3
+}
+// console.log(sumThreeNumber(4,5,2)) 11
+
+storeinvar=sumThreeNumber(4,5,3) //12
+console.log(storeinvar)
+
+function Even(num){
+//     if(num%2==0){
+//     return true}
+//     else {
+//   return false
+//     }
+return num%2==0
+    
+}
+console.log(Even(3))
