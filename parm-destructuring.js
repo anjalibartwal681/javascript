7de0ca7e331a49d7184427803e32568adbf0c03d
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-02 18:05:57 +0000

    Create parm-destructuring.js

---
+++ parm-destructuring.js
@@ -0,0 +1,13 @@
+//parm  destructuring
+//object 
+//react
+const person={
+    firstName:"harshit",
+    gender:"male"
+}
+function printdetails(obj){
+    console.log(obj.firstName)
+        console.log(obj.gender)
+}
+
+ printdetails(person)
