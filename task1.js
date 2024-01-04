30a2e70647bf7330ea9c48d93f8d0195c1f1304b
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-04 10:39:56 +0000

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,25 @@
+map
+filter
+reduce
+settimeout
+setinterval
+for
+while
+do,while
+JSON.parse
+JSON.stringify
+date functijon
+time function
+year,month,day
+house,minutes,seconds in date funtion
+
+1
+12
+123
+1234
+12345
+*
+**
+***
+****
+*****
