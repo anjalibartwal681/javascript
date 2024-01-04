93736c0ebb107e455e0a56f0550761c709983776
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-04 10:07:12 +0000

    Create for.js

---
+++ for.js
@@ -0,0 +1,24 @@
+// 1
+// 12
+// 123
+// 1234
+// 12345
+let num=0;
+for(let i=1;i<=5;++i){
+   
+    for(let j=1;j<=i;++j){
+        console.log(j)
+
+    }
+    console.log("\n") }
+
+
+
+//while
+let i=0
+while(i<10){
+    
+   i++ 
+   console.log("not having error")
+    
+}
