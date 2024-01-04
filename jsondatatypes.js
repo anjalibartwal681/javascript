7b14e97e043782c584abaf2b95a3051b1da132ef
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-04 11:07:22 +0000

    Create jsondatatypes.js

---
+++ jsondatatypes.js
@@ -0,0 +1,13 @@
+// JSON Data Types
+{"name":"anjari"}
+JSON Numbers
+
+
+{"age":"23"}
+
+//json object
+"employee":{
+"name":"mariya",["address":{tokyo},{japan}]
+}
+
+"employee":["anna","rhn","anh"]
