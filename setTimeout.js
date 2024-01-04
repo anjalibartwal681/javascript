8477ecca845bcec218d6ef3b792608d1d7017020
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-04 07:53:18 +0000

    Create setTimeout.js

---
+++ setTimeout.js
@@ -0,0 +1,12 @@
+//setTimeout
+
+// The setTimeout() method calls a function after a number of milliseconds.
+//const myTimeout = setTimeout(fun, timein millisecond  5000);
+
+let settime= setTimeout(greetings,3000);
+
+function greetings(){
+  a="anata ksukete";
+  console.log(a)
+  return a;
+}
