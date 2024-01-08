b237fd22046552a7d7dc67f1d4302b1c6c93baf7
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-08 07:47:41 +0000

    Create paterrn.js

---
+++ paterrn.js
@@ -0,0 +1,46 @@
+
+/// program for printing 
+// 1
+// 12
+// 123
+// 1234
+// 12345
+let space=''
+for(let i=1;i<=5;i++)
+{    for(let j=1;j<=i;j++)
+    {          space += j
+          }
+    space +="\n"
+   }
+console.log(space)
+////
+// *
+// **
+// ***
+// ****
+// *****
+let space=''
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=i;j++){
+        space+="*"
+    }
+    space+="\n"
+}
+console.log(space)
+//pattern
+let space=''
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=i;j++){
+        space+=i
+    }
+    space+="\n"
+}
+console.log(space)
+
+
+
+
+
+
