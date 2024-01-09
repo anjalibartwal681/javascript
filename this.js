fe15f474bf70ee9e3bba05ecaf57e8600ce88d1d
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-09 10:46:21 +0000

    Create this.js

---
+++ this.js
@@ -0,0 +1,15 @@
+this keywoed has differnt role depend up on its piece of code where is this going use
+
+
+
+function this(){
+    
+    
+    this  subsitution  if the value of this keyword  is undefined land null it will be   replace it global object only on non strict mode. value become window object.
+    }
+    value of this keyword depend on how we call it
+    //when we call  function
+            this keyword inside  object 
+            
+            
+             
