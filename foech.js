3eea4fe6236bef3077010318001605cde58aa00a
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-09 11:26:11 +0000

    Create foech.js

---
+++ foech.js
@@ -0,0 +1,7 @@
+let users=[{name:"tomichan",sai:24,ue:"tokyo",kuni:"nihongo"},
+{name:"tomi",sai:24,ue:"tokyo",kuni:"nihongo"},
+{name:"chan",sai:24,ue:"tokyo",kuni:"nihongo"},
+{name:"tomichi",sai:24,ue:"tokyo",kuni:"nihongo"},]
+users.forEach((user,index)=>{
+    console.log(user.name,index)
+})
