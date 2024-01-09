f1d547fe3d23f6f27c625a1f9f7d9628abc08c90
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-09 11:51:09 +0000

    Update foech.js

--- foech.js
+++ foech.js
@@ -5,3 +5,10 @@ let users=[{name:"tomichan",sai:24,ue:"tokyo",kuni:"nihongo"},
 users.forEach((user,index)=>{
     console.log(user.name,index)
 })
+
+
+
+
+ const username=users.map((users ,index)=>{
+return  users.name})
+console.log(username)
