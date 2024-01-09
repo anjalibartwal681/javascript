b560dfbe9e9e68f197aa8f5df7e557560ba8cba0
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-09 08:14:38 +0000

    Create pattern.js

---
+++ html/pattern.js
@@ -0,0 +1,58 @@
+// let space=''
+
+// for(let i=1;i<=5;i++){
+    
+//     for(let j=1;j<=i;j++){
+        
+//     space += "*"
+    
+//   }
+  
+//     space +="\n"
+    
+    
+// }
+// console.log(space)
+
+
+
+                                                   
+let space=''     
+                    
+
+for(let i=5;i>=1;i--){
+    
+    for(let j=i;j>=1;j--){
+        
+    space += j
+    
+  }
+  
+    space +="\n"
+    
+    
+}
+console.log(space)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
