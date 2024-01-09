2b0b2ac3539801bb948924445f9488a7fdf3c46e
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-09 11:32:30 +0000

    Create map1.js

---
+++ map1.js
@@ -0,0 +1,11 @@
+//map method
+let numbers=[2,3,4,5,6]
+// square=(number)=>{
+//  return number*number   
+// }
+
+
+let squarenumbers=numbers.map((number)=>{
+    return number*number
+})
+console.log(squarenumbers)
