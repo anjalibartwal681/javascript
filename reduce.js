26a2255ad34ca1457ba9dce5bc336f27668d823e
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-09 12:12:24 +0000

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -18,3 +18,13 @@ function myFunc(total, num) {
 
 
 
+//reduce method
+let numbers=[1,2,3,4,5,9,10]
+
+
+const  sum = numbers.reduce((accumulator,currentvalue)=>{
+    return accumulator+currentvalue
+})
+console.log(sum)
+
+
