f98f732311eef2d395e90943bad7a8c42d01a2e4
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-11 10:53:50 +0000

    Update asyncawait.js

--- asyncawait.js
+++ asyncawait.js
@@ -9,14 +9,84 @@ async function myDisplay() {
 }
  
  myDisplay() // output watashi!!
+
+
+
+
+
  
  // we use await cause when we use lots of then .then it becomes callback hell so we use  async  await 
  // javascript   .
  //settimeout send the request to the browser 
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+let empty_tank = function(){
 
+    return new Promise(function(resolve,reject){
 
+         resolve("The car doesn't have enough fuel.")
+
+     })
+
+ }
+ let engine = function(msg){
+
+     return new Promise(function(resolve,reject){
+
+         resolve(msg + "The engine is over heating.")
+
+     })
+
+ }
+
+ let travel = function(msg){
+
+     return new Promise(function(resolve,reject){
+
+         resolve(msg + "The car is not safe for driving.")
+
+     })
+
+ }
+
+ const myCar=async()=>{
+
+ 
+
+const tankResult=await empty_tank();
+const engineResult= await engine(tankResult)
+const travelResult= await travel(engineResult)
+console.log("Done all promises",travelResult)
+// empty_tank().then(function(result){
+
+//      return engine(result)
+
+//  }).then(function(result){
+
+//      return travel(result)
+
+//  }).then(function(result){
+
+//      console.log("Done!" + result)
+
+//  })
+
+ }
+ myCar()
+function greet(){
+    return emp = new Promise((resolve, reject)=>{
+        resolve("anata o genki desu ka")})
+        
+    }
+    
+    const ngreet =async()=>{
+    let c = await greet()
+    console.log(c)
+        
+    }
+    ngreet()// output      anata o genki desu ka
+      .........................................................................
 
 // a promise
 let promise = new Promise(function (resolve, reject) {
