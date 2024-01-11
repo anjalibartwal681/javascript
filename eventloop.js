8d6c1471f07ffdb8025c12d1d78245e335ab924d
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-11 12:24:07 +0000

    Create eventloop.js

---
+++ eventloop.js
@@ -0,0 +1,14 @@
+function a(){
+    console.log("hi")
+  
+    setTimeout(()=>{
+        console.log("im good")
+    },2000)
+      b()
+      console.log("imbambmnknv")
+    
+}
+function b(){
+    console.log("hit")
+}
+a()
