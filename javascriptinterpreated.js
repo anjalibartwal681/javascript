19f5ce01b5921ae94cf29de32a6714cd107dadf0
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-12 10:01:28 +0000

    Create javascriptinterpreated.js

---
+++ javascriptinterpreated.js
@@ -0,0 +1,10 @@
+javascript is interpreated language  it execute code line by line  but having   modern js engine v8 engine used in google chrome  and node js  use a combination of interpertation and compilation
+to improve performance of javascript  execution  time.
+  Interpretation: The JavaScript engine initially interprets the source code line by line. This allows for quick startup and execution of the code.
+
+Compilation: Many modern JavaScript engines employ Just-In-Time (JIT) compilation. Instead of interpreting the entire code upfront, the engine may 
+  analyze the code and convert parts of it into machine code just before they are executed. This compiled machine code can be executed more efficiently than the original source code.
+
+Optimizations: JavaScript engines also perform various optimizations to improve the execution speed. This may include inlining functions, eliminating dead code, 
+    and applying other techniques to enhance the overall performance of the code. 
+    
