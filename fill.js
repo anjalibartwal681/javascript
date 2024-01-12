f0ca310e4326c445494623618f5d224b3212ae80
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-12 18:17:24 +0000

    Create fill.js

---
+++ fill.js
@@ -0,0 +1,15 @@
+//fill method
+//value start end
+// //fill change original array
+// const myarray= new Array(10).fill(-1)
+// console.log(myarray)
+const myarray=[1,2,3,4,5,6,7,8]
+myarray.fill(0,2,4)
+console.log(myarray)
+// output
+
+[
+  1, 2, 0, 0,
+  5, 6, 7, 8
+]
+
