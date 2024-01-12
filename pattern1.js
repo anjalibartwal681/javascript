cdfd5dde5cfd9d856cbd38f6b68ab7894d16a747
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-12 05:51:22 +0000

    Update pattern1.js

--- pattern1.js
+++ pattern1.js
@@ -3,3 +3,29 @@ for(var i=1;i<2;i++)
     console.log(i);
 }
 console.log(i);
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+    let str=""
+
+for(i=0;i<2;i++){
+    for(j=0;j<2;j++){
+    
+        
+        str+=i
+    }
+
+console.log(str+="\n")
+}
+console.log(str)
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+console.log("This is line 1\nThis is line 2");
+//output
+// This is line 1
+// This is line 2
+
+var a=1;
+console.log(++a); // 1 2
+console.log(++a);// 3
+console.log(a); //3
