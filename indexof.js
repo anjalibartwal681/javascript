bc062e6a71ab0391a7b27952398e22c60282e71e
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-12 08:06:50 +0000

    Update indexof.js

--- indexof.js
+++ indexof.js
@@ -1,6 +1,21 @@
+// indexof doesn't change the original array
+//indexof method returns -1 if values not found
+
 <script>
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let index = fruits.indexOf("Apple");
 
 document.getElementById("demo").innerHTML = index;
 </script>
+
+
+
+<p id="demo"></p>
+
+<script>
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Apple"];
+let index = fruits.indexOf("Apple", -1);//-1 found from last
+                                                                //  output 4 
+document.getElementById("demo").innerHTML = index;
+</script>
+
