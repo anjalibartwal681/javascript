e26116f735213cb7b56f9493dc5be1cc6f03f394
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-16 07:35:27 +0000

    Update eventloop.js

--- eventloop.js
+++ eventloop.js
@@ -1,3 +1,8 @@
+In JavaScript, the setTimeout() function is utilized to introduce a delay or to execute
+    a particular function after a specified amount of time has passed. It is part of the Web APIs provided by browsers and Node. 
+    js, allowing asynchronous execution of code.8
+
+
 function a(){
     console.log("hi")
   
