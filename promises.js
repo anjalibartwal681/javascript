bc3d3548b78eb9c33f286c15c39c57f4dcfc558d
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-16 06:00:06 +0000

    Update promises.js

--- promises.js
+++ promises.js
@@ -19,6 +19,24 @@ let myPromise = new Promise((resolve,reject)=>{
  
 
 
+--------------------------------------Promise chaining--------------------------------------------------------------------------
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise
+  .then(function (result) {
+    alert(result); // 2
+    return result * 2;
+  })
+  .then(function (result) {
+    alert(result); // 4
+    return result+1;
+  })
+  .then(function (result) {
+    alert(result) // 16
+    return result
+  });
 
 
 
