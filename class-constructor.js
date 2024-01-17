6f31f237e3f990af7bd3aaab6d63da794240928b
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-17 06:36:15 +0000

    Update class-constructor.js

--- class-constructor.js
+++ class-constructor.js
@@ -1,3 +1,17 @@
+class ClassName {
+  constructor() { ... }
+}
+
+The Constructor Method
+The constructor method is a special method:
+
+It has to have the exact name "constructor"
+It is executed automatically when a new object is created
+It is used to initialize object properties
+If you do not define a constructor method, JavaScript will add an empty constructor method.
+
+
+
 class MyClass {
   // No constructor defined
 
@@ -21,3 +35,21 @@ myObject.setValues("Hello", 42);
 
 // Create an instance of MyClass with values
 const myObject = new MyClass("Hello", 42);
+----------------------------------------------------------------------------------------------------------------------------------------
+ //inheritance
+  class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+class b extends Car
+{
+    display()
+    {
+        console.log(this.name,this.year);
+        
+    }
+}
+const ob=new b("ford",2014);
+ob.display();
