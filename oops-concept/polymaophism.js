af0e0d734548fcb5f1716a0bedb99f180321eafd
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-17 18:54:24 +0000

    Create polymaophism.js

---
+++ oops-concept/polymaophism.js
@@ -0,0 +1,20 @@
+class shape(){
+    draw(){
+        return "im genric shape"
+    }
+}
+
+class square extends shape{
+    draw(){
+        return "im square"
+    }
+}
+
+
+class circle extends shape{
+    draw(){
+        return "im circle"
+    }
+}
+
+ob= new shape()
