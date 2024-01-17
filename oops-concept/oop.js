2173c64f09a7a9b42dbff3350f9c989ffc2565d9
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-17 10:48:51 +0000

    Update oop.js

--- oops-concept/oop.js
+++ oops-concept/oop.js
@@ -5,7 +5,16 @@ As JavaScript is widely used in Web Development, in this article we will explore
 Object	      Classes  	       Encapsulation
 Abstraction 	Inheritance  	Polymorphism
 
+let obj={
+    name:"anjali",
+    cls:"bscit",
+    getfun:function(){
+        return `${obj.name} done ${obj.cls}`
+    }
+};
 
+console.log(obj.name)
+console.log(obj.getfun())
 
 Object: An Object is a unique entity that contains properties and methods. For example “a car” is a real-life Object, which has some characteristics like color, type, model, and horsepower and performs certain actions like driving. The characteristics of an Object are called Properties in Object-Oriented Programming and the actions are called methods. An Object is an instance of a class. Objects are everywhere in JavaScript, almost every element is an Object whether it is a function, array, or string. 
 
