3bba1e6338bb95881fb6a36ad9b18d105b46586c
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-17 09:29:43 +0000

    Create oop.js

---
+++ oops-concept/oop.js
@@ -0,0 +1,4 @@
+
+Introduction to Object Oriented Programming in JavaScript
+
+As JavaScript is widely used in Web Development, in this article we will explore some of the Object Oriented mechanisms supported by JavaScript to get the most out of it. Some of the common interview questions in JavaScript on OOPS include:
