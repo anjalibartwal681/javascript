76acc1bed645086c039dc79e4d1b36f75436ee43
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-18 07:07:50 +0000

    Update polymaophism.js

--- oops-concept/polymaophism.js
+++ oops-concept/polymaophism.js
@@ -1,5 +1,38 @@
 polymorphism allows you to use a single function or method name to perform different actions 
     based on the types of arguments or objects involve
+ polymorphism refers to the ability of a single function or method to work with different types of objects or data.
+ ex -   dr who cure nose as well as ear and throat
+
+class Shape {
+    draw() {
+        console.log('Drawing a generic shape');
+    }
+}
+
+class Circle extends Shape {
+    draw() {
+        let arr=[5,6,7,8,9,]
+    let  c=arr.reduce((accumu,total)=>{
+          return accumu+total
+      })
+       console.log(c)
+    }
+}
+
+class Square extends Shape {
+    draw() {
+        console.log('Drawing a square');
+    }
+}
+
+circle= new Shape()
+circle.draw()
+circle= new Circle()
+circle.draw()
+
+
+
+ 
 
 
 
