94d92db932dc6ab333f83856977f39c8a8c3e643
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-18 10:09:21 +0000

    Update public-private-procted.js

--- oops-concept/public-private-procted.js
+++ oops-concept/public-private-procted.js
@@ -21,3 +21,42 @@ const myprotect = new Myprotect()
 console.log(myprotect.getPrtect());
 
 
+
+--------------------------------------------------------------------------------------------------------------------
+class Counter {
+  _count = 5; 
+display()
+{
+    console.log(this._count);
+}
+}
+
+class b extends Counter
+{
+    display1()
+    {
+        console.log(this._count);
+    }
+}
+class c extends b
+{
+    display2()
+    {
+                console.log(this._count);
+    }
+}
+const ob=new c();
+ob.display2();
+ob.display2();
+ob.display();
+ console.log(ob._count)
+
+
+
+
+
+
+
+  
+
+
