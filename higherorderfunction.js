d376ede232438df910bc096b22fc2e866e2927d8
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-02-02 06:02:04 +0000

    Update higherorderfunction.js

--- higherorderfunction.js
+++ higherorderfunction.js
@@ -8,3 +8,22 @@ function callbackFunction() {
 }
 
 higherOrderFunction(callbackFunction)
+
+
+function higherOrderFunction(callback) {
+    console.log("iam higherorder function");
+    
+    callback();
+    
+    console.log("Tasks after calling the callback");
+}
+
+// Define a callback function
+function callbackFunction() {
+    console.log("Callback function executed");
+}
+
+// Call the higher-order function with the callback
+higherOrderFunction(callbackFunction);
+
+
