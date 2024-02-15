373ae4b4795c99a83e18f574f81a080cbb2cd5e5
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-02-15 11:31:49 +0000

    Update Deep-copy.js

--- Deep-copy.js
+++ Deep-copy.js
@@ -15,5 +15,27 @@ newEmployee.salary=100000;
 console.log(employee)
 console.log(newEmployee)
 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+let employee = {
+	eid: "E102",
+	ename: "Jack",
+	eaddress: "New York",
+	salary: 50000
+}
+
+let newEmployee = JSON.parse(JSON.stringify(employee)); 
+console.log(newEmployee===employee);
+// newEmployee.ename="anjali";
+// newEmployee.salary=100000;
+// console.log(newEmployee)
+// console.log(employee)
+
+
+
+
+
 
 
