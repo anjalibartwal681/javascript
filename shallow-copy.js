b9ca4fbe37a21345493b78809019341c344554b9
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-02-15 11:14:55 +0000

    Create shallow-copy.js

---
+++ shallow-copy.js
@@ -0,0 +1,15 @@
+let employee = {
+	eid: "E102",
+	ename: "Jack",
+	eaddress: "New York",
+	salary: 50000
+}
+
+let newEmployee = employee; 
+newEmployee.ename="anjali";
+newEmployee.salary=100000;
+console.log(newEmployee)
+console.log(employee)
+
+
+
