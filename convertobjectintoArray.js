714458d01e1e6c6cbb85401a991dacfdcad660e4
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-03-12 16:07:36 +0530

    Create convertobjectintoArray.js

---
+++ convertobjectintoArray.js
@@ -0,0 +1,10 @@
+const old = {
+  name: 'Piyush',
+  email: 'Piyush@gmail',
+};
+
+const objKeys = Object.keys(old);
+const objValues=Object.values(old);
+console.log(">>>>keys",objKeys);
+console.log(">>>>vslyues",objValues);
+
