fd8d753988c3242752a1f591458c10b4373197e2
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-03-28 20:39:29 +0530

    UpdateArrayQuestion.js

--- arrayQuestion.js
+++ arrayQuestion.js
@@ -17,3 +17,62 @@ const abc = (arr1, arr2) => {
 console.log(abc(a[0], b[0]));
 console.log(abc(b[0], c[0]));
 
+***************************************************************************************************
+    const a = ["mumm", "boy"];
+const b = ["mumm", "boy"];
+const c = ["allien", "line"];
+const d = ["vooodo", "vooo"];
+
+const abc = (arr1,arr2) => {
+let find=false
+    arr1.map((item1) => {
+     arr2.map((item2)=>{
+         if(item1===item2){
+         find = true    
+         }
+     });
+    
+    });
+  return find
+}
+
+console.log(abc(a,b));
+// abc(b);
+// abc(c);
+// abc(d);
+
+// *********************************************
+
+
+const a = ["mumm", "boy"];
+const b = ["mumm", "boy"];
+const c = ["allien", "line"];
+const d = ["vooodo", "vooo"];
+
+const abc = (arr1, arr2) => {
+    let find = false;
+    arr1.forEach((item1) => { 
+        arr2.forEach((item2) => { 
+            if (item1 === item2) { 
+                find = true; 
+            }
+        });
+    });
+    return find; // Return the boolean indicating if any matches were found
+}
+
+console.log(abc(a, b)); // Output: true (since "mumm" is present in both arrays)
+// abc(b);
+// abc(c);
+// abc(d);
+
+
+
+
+
+
+
+
+
+
+
